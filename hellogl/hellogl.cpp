6ff50aa7c5588e6eeb4f88ec39f6054173991b41
chinjja <chinjja@gmail.com>
2021-04-08 09:32:03 +0900

    modern opengl hint

--- hellogl/hellogl.cpp
+++ hellogl/hellogl.cpp
@@ -54,8 +54,10 @@ int main()
         exit(EXIT_FAILURE);
     }
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_SAMPLES, 4);
 
     window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
     if (!window)
